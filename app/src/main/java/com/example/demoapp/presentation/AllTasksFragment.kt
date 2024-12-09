3857f65e245b3bacb5a89c3eba73933fcc62dd1e
Olamide Adeleye <olamide.adeleye@babbangona.com>
2024-12-09 09:35:16 +0100

    Modify presentation layer

--- app/src/main/java/com/example/demoapp/presentation/AllTasksFragment.kt
+++ app/src/main/java/com/example/demoapp/presentation/AllTasksFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.demoapp.R
-import com.example.demoapp.databinding.FragmentFirstBinding
+import com.example.demoapp.databinding.FragmentAllTasksBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class AllTasksFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentAllTasksBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentAllTasksBinding.inflate(inflater, container, false)
         return binding.root
 
     }
--- app/src/main/java/com/example/demoapp/presentation/EditTaskFragment.kt
+++ app/src/main/java/com/example/demoapp/presentation/EditTaskFragment.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.demoapp.R
-import com.example.demoapp.databinding.FragmentSecondBinding
+import com.example.demoapp.databinding.FragmentEditTaskBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class EditTaskFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentEditTaskBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentEditTaskBinding.inflate(inflater, container, false)
         return binding.root
 
     }
