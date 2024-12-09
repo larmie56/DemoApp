6cc56233c65b3d7caf3ca4d5682adddd3e367d82
Olamide Adeleye <olamide.adeleye@babbangona.com>
2024-12-09 05:58:46 +0100

    Create usecases

---
+++ app/src/main/java/com/example/demoapp/domain/CreateTaskUseCase.kt
@@ -0,0 +1,6 @@
+package com.example.demoapp.domain
+
+import com.example.demoapp.data.repo.TasksRepo
+
+class CreateTaskUseCase(tasksRepo: TasksRepo) {
+}
---
+++ app/src/main/java/com/example/demoapp/domain/EditTaskUseCase.kt
@@ -0,0 +1,6 @@
+package com.example.demoapp.domain
+
+import com.example.demoapp.data.repo.TasksRepo
+
+class EditTaskUseCase(tasksRepo: TasksRepo) {
+}
